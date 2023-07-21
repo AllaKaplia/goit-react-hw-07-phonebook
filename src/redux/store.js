f5b80e5fc9e674d526d9408c25f56ddc1b087ac5
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-22 00:48:04 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,27 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
-import {
-    persistStore,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-} from 'redux-persist';
-import {contactsReducer} from './contactsSlice';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { contactsReducer } from './contactsSlice';
+import { filterReducer } from './filterSlice';
 
 
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterReducer,
+});
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: rootReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
