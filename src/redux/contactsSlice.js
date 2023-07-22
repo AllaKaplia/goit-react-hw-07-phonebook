6036a328e95d93565db0de7385069729606949e6
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-23 00:42:41 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -38,8 +38,10 @@ const contactsSlice = createSlice({
       .addCase(removeContact.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
-        const index = state.items.findIndex((contact) => contact.id === action.payload);
-        state.items.splice(index, 1, action.payload);
+        const index = state.items.findIndex(
+          contact => contact.id === action.payload.id
+        );
+        state.items.splice(index, 1);
       })
       .addCase(removeContact.rejected, handleRejected);
   },
