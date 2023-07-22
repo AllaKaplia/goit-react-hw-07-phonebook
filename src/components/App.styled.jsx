c745a048576437be67f2e266e046197e463957d5
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-22 16:37:48 +0300

    Update App.styled.jsx

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -1,9 +1,9 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
-    width: 900px;
+    width: 600px;
     text-align: center;
-    background-color: bisque;
+    background-color: #9ce9b1;
     margin: 0 auto;
     margin-top: 30px;
     border-radius: 10px;
